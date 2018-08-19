bfa436512e007b5387ca01fafe8e99a8cb04805d
sagar.c <sagar.c@productivet.com>
2018-08-19 13:47:16 -0500

    issue fix

--- models/module.js
+++ models/module.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose'),
 
     moduleSchema = new mongoose.Schema({
-        title: { type: String, required: true }
+        title: { type: String, required: true },
+        module: { type: String, required: true }
     });
 
 module.exports = mongoose.model('module', moduleSchema);
